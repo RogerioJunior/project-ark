c31560ec19edb7e18b0a0b1763505d36e950711a
RogerioJunor <rogeriopcjr@gmail.com>
2022-01-30 23:19:21 -0300

    page playMine

--- frontend/src/pages/PlayMine/index.js
+++ frontend/src/pages/PlayMine/index.js
@@ -4,8 +4,28 @@ function PlayMine() {
     return (
         <>
             <div className="playMine">
-                <div className="container">
+                <div className="container-play">
+                    <div className="border-container">
 
+
+                        <div className="content-left">
+                            <div className="ship-img"></div>
+                            <span className="isEmpty">EMPTY</span>
+                        </div>
+                        <div className="content-left">
+                            <div className="ship-img"></div>
+                            <span className="isEmpty">EMPTY</span>
+                        </div>
+                        <div className="content-left">
+                            <div className="ship-img"></div>
+                            <span className="isEmpty">EMPTY</span>
+                        </div>
+                        <div className="content-left">
+                            <div className="ship-img"></div>
+                            <span className="isEmpty">EMPTY</span>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </>
