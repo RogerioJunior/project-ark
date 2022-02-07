d713bbacc4bd4cb46a68bc2ea12feae4340751c1
RogerioJunor <rogeriopcjr@gmail.com>
2022-02-07 05:43:19 -0300

    refactoring

--- frontend/src/components/NavBarStore/index.js
+++ frontend/src/components/NavBarStore/index.js
@@ -3,35 +3,17 @@ import './styles.css';
 function NavBarStore() {
 
 
-    function handleClick() {
-        console.log('Click happened');
-
-    };
 
     return (
         <>
-            <div className="NavBarStore justify-content-center">
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#" onClick={handleClick}>
-                                <span className="titles">SHIPS</span></a>
-                        </li>
-                        <li>
-                            <a href="#" onClick={handleClick}>
-                                <span className="titles">CONSUMABLES</span></a>
-                        </li>
-                        <li>
-                            <a href="#" onClick={handleClick}>
-                                <span className="titles">UPGRADE</span></a>
-                        </li>
-                        <li>
-                            <a href="#" onClick={handleClick}>
-                                <span className="titles">RESOURCE</span></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
+        <div className="container">
+            <div className="nav-bar-store col-12 d-flex justify-content-around text-center">
+                <a className="nav-item nav-link" href="#">SHIPS </a>
+                <a className="nav-item nav-link" href="#">CONSUMABLES</a>
+                <a className="nav-item nav-link" href="#">UPGRADE</a>
+                <a className="nav-item nav-link" href="#">RESOURCE</a>
+                </div>
+                </div>
         </>
     )
 }
